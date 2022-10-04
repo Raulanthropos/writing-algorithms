398d93324783f5025497c599cbc9029ef2243aa4
Raulopoulos <62166476+Raulopoulos@users.noreply.github.com>
2022-10-04 17:52:50 +0300

    Homework 04.10

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstName = "Ioannis";
+let middleName = "Raul";
+let lastName = "Psychias";
+let space = " ";
+let fullName = firstName + space + lastName;
+console.log("Hello, my name is", fullName);
+
+let age = 34;
+let currentYear = 2022;
+let birthYear = currentYear - age;
+console.log("The year I was born was", birthYear);
---
+++ recipe.js
@@ -0,0 +1,8 @@
+let spaghetti = 400;
+let guanciale = 250;
+let veryFreshEggYolks = 6;
+let agedPecorinoRomano = 50;
+let blackPeppper = 4;
+let smallBowl = veryFreshEggYolks + agedPecorinoRomano;
+let carbonara = smallBowl + spaghetti + guanciale + blackPeppper;
+console.log(carbonara)
